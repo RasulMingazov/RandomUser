b5bda8cdb8b314b070698df926537868b1d01127
Rasul Mingazov <rasul.mingazov@gmail.com>
2022-03-25 00:02:16 +0300

    Add dependencies

--- app/src/main/java/com/jeanbernad/randomuser/ui/MainActivity.kt
+++ app/src/main/java/com/jeanbernad/randomuser/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.jeanbernad.randomuser
+package com.jeanbernad.randomuser.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jeanbernad.randomuser.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
