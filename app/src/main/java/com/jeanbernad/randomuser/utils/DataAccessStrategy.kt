5634bc697dc2514539d18142d932a66c2a206f50
Rasul Mingazov <rasul.mingazov@gmail.com>
2022-03-28 20:01:41 +0300

    Update DataAccessStrategy.kt

--- app/src/main/java/com/jeanbernad/randomuser/utils/DataAccessStrategy.kt
+++ app/src/main/java/com/jeanbernad/randomuser/utils/DataAccessStrategy.kt
@@ -16,15 +16,17 @@ fun <T> performGetOperation(call: suspend () -> Resource<T>):
 fun <T, A> performGetOperation(databaseQuery: () -> LiveData<T>,
                                networkCall: suspend () -> Resource<A>,
                                saveCallResult: suspend (A) -> Unit): LiveData<Resource<T>> =
-        liveData(Dispatchers.IO) {
-            emit(Resource.loading())
-            val source = databaseQuery.invoke().map { Resource.success(it) }
-            val responseStatus = networkCall.invoke()
-            if (responseStatus.status == Resource.Status.SUCCESS) {
-                saveCallResult(responseStatus.data!!)
-            } else if (responseStatus.status == Resource.Status.ERROR) {
-                emit(Resource.error(responseStatus.message!!))
-                emitSource(source)
-            }
-            emitSource(source)
+    liveData(Dispatchers.IO) {
+        emit(Resource.loading())
+        val source = databaseQuery.invoke().map { Resource.success(it) }
+
+        val responseStatus = networkCall.invoke()
+        if (responseStatus.status == Resource.Status.SUCCESS) {
+            saveCallResult(responseStatus.data!!)
+
+        } else if (responseStatus.status == Resource.Status.ERROR) {
+            emit(Resource.error(responseStatus.message!!))
         }
+        emitSource(source)
+
+    }
