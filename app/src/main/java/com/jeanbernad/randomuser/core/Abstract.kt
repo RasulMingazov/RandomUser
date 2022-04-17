59d9bd72ad21ce5e538ed8c3a563fed08958dc63
Rasul Mingazov <rasul.mingazov@gmail.com>
2022-04-18 02:54:28 +0300

    Remove unused

--- app/src/main/java/com/jeanbernad/randomuser/core/Abstract.kt
+++ app/src/main/java/com/jeanbernad/randomuser/core/Abstract.kt
@@ -2,10 +2,6 @@ package com.jeanbernad.randomuser.core
 
 abstract class Abstract {
 
-    interface DataObject
-
-    interface CloudObject
-
     interface Object<T, M : Mapper> {
         fun map(mapper: M): T
     }
@@ -15,15 +11,6 @@ abstract class Abstract {
         interface Data<S, R> : Mapper {
             fun map(data: S): R
         }
-
-        interface DataToDomain<S, R> : Data<S, R> {
-            fun map(e: Exception): R
-        }
-
-        interface DomainToUi<S, T> : Data<S, T> {
-            fun map(errorType: ErrorType): T
-        }
-
         class Empty : Mapper
     }
 }
