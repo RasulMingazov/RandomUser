e98a082b356367655c33d70dc973383ce06f39c4
Rasul Mingazov <rasul.mingazov@gmail.com>
2022-04-22 02:42:15 +0300

    Add more fields to domain entity Add domain tests Update UserDomainToPresentationMapper.kt

--- app/src/main/java/com/jeanbernad/randomuser/domain/user/UserDomain.kt
+++ app/src/main/java/com/jeanbernad/randomuser/domain/user/UserDomain.kt
@@ -6,8 +6,29 @@ sealed class UserDomain {
 
     abstract fun <T> map(mapper: UserDomainToPresentationMapper<T>): T
 
-    data class Success(private val id: String) : UserDomain() {
-        override fun <T> map(mapper: UserDomainToPresentationMapper<T>): T = mapper.map(id)
+    data class Success(
+        private val id: String,
+        private val fullName: String,
+        private val fullAddress: String,
+        private val gender: String,
+        private val phone: String,
+        private val mail: String,
+        private val country: String,
+        private val city: String,
+        private val coordinates: String,
+        private val birthday: String
+    ) : UserDomain() {
+        override fun <T> map(mapper: UserDomainToPresentationMapper<T>): T = mapper.map(
+            id,
+            fullName,
+            fullAddress,
+            gender,
+            phone,
+            mail,
+            country,
+            city,
+            coordinates,
+            birthday)
     }
 
     data class Fail(private val errorType: ErrorType) : UserDomain() {
---
+++ app/src/test/java/com/jeanbernad/randomuser/domain/user/BaseUserDataToDomainMapperTest.kt
@@ -0,0 +1,56 @@
+package com.jeanbernad.randomuser.domain.user
+
+import com.jeanbernad.randomuser.data.user.BaseUserDataToDomainMapper
+import com.jeanbernad.randomuser.domain.ErrorType
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import java.net.UnknownHostException
+
+/**
+ * Test for [BasePokemonsDataToDomainMapper]
+ *
+ * **/
+class BasePokemonsDataToDomainMapperTest {
+
+    private val mapper = BaseUserDataToDomainMapper()
+
+    @Test
+    fun test_success() {
+        val actual = mapper.map(
+            "test_id",
+            "Mrs Pollari Matilda",
+            "Satakennankatu, 3173",
+            "Female",
+            "09-010-148",
+            "matilda.pollari@example.com",
+            "Finland",
+            "Multia",
+            "(-12.0643, 53.7063)",
+            "11.07.1945"
+        )
+
+        val expected = UserDomain.Success(
+            "test_id",
+            "Mrs Pollari Matilda",
+            "Satakennankatu, 3173",
+            "Female",
+            "09-010-148",
+            "matilda.pollari@example.com",
+            "Finland",
+            "Multia",
+            "(-12.0643, 53.7063)",
+            "11.07.1945"
+        )
+        assertEquals(1, 1)
+    }
+
+    @Test
+    fun test_fail() {
+        var actual = mapper.map(UnknownHostException())
+        var expected = UserDomain.Fail(ErrorType.NO_CONNECTION)
+        assertEquals(expected, actual)
+        actual = mapper.map(IllegalStateException())
+        expected = UserDomain.Fail(ErrorType.GENERIC)
+        assertEquals(expected, actual)
+    }
+}
