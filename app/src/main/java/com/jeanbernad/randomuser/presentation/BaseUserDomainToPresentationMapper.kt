17f02dbcf2b26b4b0163dc5abf4760cd63a8d733
Rasul Mingazov <rasul.mingazov@gmail.com>
2022-04-22 03:30:33 +0300

    Add more fields to presentation entity Add presentation tests Update BaseUserDomainToPresentationMapper.kt.kt

--- app/src/main/java/com/jeanbernad/randomuser/presentation/BaseUserDomainToPresentationMapper.kt
+++
@@ -1,7 +0,0 @@
-package com.jeanbernad.randomuser.presentation
-
-import com.jeanbernad.randomuser.domain.UserDomainToPresentationMapper
-
-class BaseUserDomainToPresentationMapper : UserDomainToPresentationMapper<UserPresentationModel> {
-    override fun map(id: String) = UserPresentationModel.Base(id)
-}
---
+++ app/src/test/java/com/jeanbernad/randomuser/presentation/user/BaseUserDomainToPresentationMapperTest.kt
@@ -0,0 +1,45 @@
+package com.jeanbernad.randomuser.presentation.user
+
+import com.jeanbernad.randomuser.R
+import com.jeanbernad.randomuser.core.ErrorPresentationMapper
+import com.jeanbernad.randomuser.core.ResourceProvider
+import com.jeanbernad.randomuser.domain.ErrorType
+import org.junit.Test
+import org.junit.Assert.assertEquals
+
+
+class BaseUserDomainToPresentationMapperTest {
+
+    private val resourceProvider = TestResourceProvider()
+    private val errorPresentationMapper = ErrorPresentationMapper.Base(resourceProvider)
+    private val mapper = BaseUserDomainToPresentationMapper(errorPresentationMapper)
+
+    @Test
+    fun test_fail_no_connection() {
+        val actual = mapper.map(ErrorType.NO_CONNECTION)
+        val expected = UserPresentationModel.Fail("no_connection")
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun test_fail_server_not_available() {
+        val actual = mapper.map(ErrorType.SERVICE_UNAVAILABLE)
+        val expected = UserPresentationModel.Fail("server_not_available")
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun test_fail_something_go_wrong() {
+        val actual = mapper.map(ErrorType.GENERIC)
+        val expected = UserPresentationModel.Fail("something_go_wrong")
+        assertEquals(expected, actual)
+    }
+
+    private inner class TestResourceProvider : ResourceProvider {
+        override fun getString(id: Int) = when (id) {
+            R.string.no_connection -> "no_connection"
+            R.string.server_not_available -> "server_not_available"
+            else -> "something_go_wrong"
+        }
+    }
+}
