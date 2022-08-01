bb3f2aec8d4cc10529a3405f81c3a6ca8a045bb8
rasulmingazov <rasul.mingazov@gmail.com>
2022-08-02 00:52:49 +0300

    Refactor LoaderImage.kt

--- app/src/main/java/com/jeanbernad/randomuser/presentation/common/LoaderImage.kt
+++ app/src/main/java/com/jeanbernad/randomuser/presentation/common/LoaderImage.kt
@@ -1,26 +1,23 @@
 package com.jeanbernad.randomuser.presentation.common
 
-import android.graphics.drawable.Drawable
 import android.widget.ImageView
 import androidx.annotation.DrawableRes
-import androidx.constraintlayout.widget.Placeholder
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DecodeFormat
 import com.bumptech.glide.load.resource.bitmap.CircleCrop
 import com.bumptech.glide.request.RequestOptions
-import com.bumptech.glide.request.target.Target
 
-interface ImageLoader {
+interface LoaderImage {
 
-    fun load(view: ImageView)
+    fun load(image: String)
 
     class BaseCircleGlide(
-        private val image: String,
+        private val view: ImageView,
         private val thumbnail: String,
         private val size: Pair<Int, Int>,
         @DrawableRes private val placeholder: Int
-    ) : ImageLoader {
-        override fun load(view: ImageView) {
+    ) : LoaderImage {
+        override fun load(image: String) {
             Glide.with(view.context)
                 .load(image)
                 .thumbnail(Glide.with(view.context).load(thumbnail))
