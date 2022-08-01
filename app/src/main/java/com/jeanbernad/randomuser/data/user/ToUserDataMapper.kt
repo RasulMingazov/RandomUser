6ad648473670df9f31a02eb3d65ab0e08008480d
rasulmingazov <rasul.mingazov@gmail.com>
2022-08-02 00:53:11 +0300

    Refactor User mapper

--- app/src/main/java/com/jeanbernad/randomuser/data/user/ToUserDataMapper.kt
+++ app/src/main/java/com/jeanbernad/randomuser/data/user/ToUserDataMapper.kt
@@ -1,10 +1,9 @@
 package com.jeanbernad.randomuser.data.user
 
 import com.jeanbernad.randomuser.core.Abstract
+import com.jeanbernad.randomuser.data.user.local.UserLocalModel
 import com.jeanbernad.randomuser.data.user.remote.UserRemoteModel
 import java.lang.Exception
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
 import java.util.*
 
 interface ToUserDataMapper : Abstract.Mapper {
@@ -13,64 +12,48 @@ interface ToUserDataMapper : Abstract.Mapper {
     ): UserData.Success
 
     fun map(
-        fullName: String,
-        fullAddress: String,
-        gender: String,
-        phone: String,
-        mail: String,
-        country: String,
-        city: String,
-        coordinates: String,
-        birthday: String,
-        image: String,
-        thumbnail: String
+        userLocalModel: UserLocalModel
     ): UserData.Success
 
     fun map(exception: Exception): UserData.Fail
 
     class Base : ToUserDataMapper {
         override fun map(userRemoteModel: UserRemoteModel) =
-            UserData.Success(
-                fullName = "${userRemoteModel.name.title} ${userRemoteModel.name.first} ${userRemoteModel.name.last}",
-                fullAddress = "${userRemoteModel.location.street.name}, ${userRemoteModel.location.street.number}",
-                gender = userRemoteModel.gender.replaceFirstChar {
-                    if (it.isLowerCase()) it.titlecase(
-                        Locale.ROOT
-                    ) else it.toString()
-                },
-                phone = userRemoteModel.phone,
-                mail = userRemoteModel.email,
-                country = userRemoteModel.location.country,
-                city = userRemoteModel.location.city,
-                coordinates = "(${userRemoteModel.location.coordinates.latitude}, ${userRemoteModel.location.coordinates.longitude})",
-                birthday = DateTimeFormatter.ofPattern("dd.MM.yyyy")
-                    .format(
-                        LocalDateTime.parse(
-                            userRemoteModel.dob.date.substring(
-                                0,
-                                userRemoteModel.dob.date.length - 1
-                            ),
-                            DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS")
-                        )
-                    ),
-                image = userRemoteModel.picture.large,
-                thumbnail = userRemoteModel.picture.thumbnail
-            )
+            with(userRemoteModel) {
+                UserData.Success(
+                    fullName = "${name.title} ${name.first} ${name.last}",
+                    fullAddress = "${location.street.name}, ${location.street.number}",
+                    gender = gender.replaceFirstChar {
+                        if (it.isLowerCase()) it.titlecase(
+                            Locale.ROOT
+                        ) else it.toString()
+                    },
+                    phone = phone,
+                    mail = email,
+                    country = location.country,
+                    city = location.city,
+                    coordinates =
+                    "(${location.coordinates.latitude}, ${location.coordinates.longitude})",
+                    birthday = dob.date,
+                    image = picture.large,
+                    thumbnail = picture.thumbnail,
+                )
+            }
 
         override fun map(
-            fullName: String,
-            fullAddress: String,
-            gender: String,
-            phone: String,
-            mail: String,
-            country: String,
-            city: String,
-            coordinates: String,
-            birthday: String,
-            image: String,
-            thumbnail: String
+            userLocalModel: UserLocalModel
         ) = UserData.Success(
-            fullName, fullAddress, gender, phone, mail, country, city, coordinates, birthday, image, thumbnail
+            fullName = userLocalModel.fullName,
+            fullAddress = userLocalModel.fullAddress,
+            gender = userLocalModel.gender,
+            phone = userLocalModel.phone,
+            mail = userLocalModel.mail,
+            country = userLocalModel.country,
+            city = userLocalModel.city,
+            coordinates = userLocalModel.coordinates,
+            birthday = userLocalModel.birthday,
+            image = userLocalModel.image,
+            thumbnail = userLocalModel.thumbnail
         )
 
         override fun map(exception: Exception) = UserData.Fail(exception)
--- app/src/main/java/com/jeanbernad/randomuser/data/user/all/local/UsersLocalMapper.kt
+++ app/src/main/java/com/jeanbernad/randomuser/data/user/all/local/UsersLocalMapper.kt
@@ -4,7 +4,7 @@ import com.jeanbernad.randomuser.core.Abstract
 import com.jeanbernad.randomuser.data.user.ToUserDataMapper
 import com.jeanbernad.randomuser.data.user.UserData
 
-interface UsersLocalMapper :Abstract.Mapper.Data<List<Abstract.Object<UserData, ToUserDataMapper>>, List<UserData>>{
+interface UsersLocalMapper : Abstract.Mapper.Data<List<Abstract.Object<UserData, ToUserDataMapper>>, List<UserData>> {
     class Base(private val toUserDataMapper: ToUserDataMapper) : UsersLocalMapper {
         override fun map(data: List<Abstract.Object<UserData, ToUserDataMapper>>) =
             data.map { upcomingEntity ->
